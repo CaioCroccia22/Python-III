958418fe44411eaab693c5aac8703e25201a9501
caiocroccia22 <caiocroccia22@gmail.com>
2024-01-16 22:09:45 -0300

    criação do arquivo

---
+++ metodo.py
@@ -0,0 +1,10 @@
+class Movie:
+    def __init__(self, name, yearLaunch, includedPlan, note, durationMinutes):
+        self.name = name
+        self.yearLaunch = yearLaunch
+        self.includedPlan = includedPlan
+        self.note = note
+        self.durationMinutes = durationMinutes
+    
+    def __str__(self):
+        return f"Filme: {self.name}"
