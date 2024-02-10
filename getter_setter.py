eb32c1c792e83620d01b1cb3442f198776fb0bf7
caiocroccia22 <caiocroccia22@gmail.com>
2024-02-09 23:00:00 -0300

    Atributos privados e getters and setters

--- getter_setter.py
+++ getter_setter.py
@@ -7,11 +7,22 @@ class Employee:
     def show(self):
         print(f"Nome {self.name} - Salario {self.__salary}")
 
+    # Como acessar e modificar valores de atributos privado
+    
+
+    #Metodo para buscar dados
+    def get_salary(self):
+        return self.__salary
+    
+    #Metodo para modificar o atributo
+    def set_salary(self, salary):
+        self.__salary = salary
+
 fulano = Employee("Fulano", 5000)
 sicrano = Employee("Sicre", 5000)
+fulano.name = "Caio Croccia"
 fulano.show()
-sicrano.show()
-fulano.__salary = 400000
-fulano.show()
+
+
 
 
